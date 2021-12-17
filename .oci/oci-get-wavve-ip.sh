da65358534241f86de873841429ead0edaae603a
ssokka <ssokka.root@gmail.com>
2021-12-17 10:53:03 +0900

    2021-12-17 10:53:04

--- .oci/oci-get-wavve-ip.sh
+++ .oci/oci-get-wavve-ip.sh
@@ -59,9 +59,9 @@ docker --version
 work_dir="$HOME/.oci"
 mkdir -p "$work_dir"
 if [[ ! -f "$work_dir/config" ]] || [[ -z `find "$work_dir/" -type f -name "*.pem"` ]]; then
-	log "Check OCI Private API Key"
-	echo "! 01. Check > https://github.com/ssokka/ubuntu/tree/master/.oci#oci-private-api-key"
-	cyn
+    log "Check OCI Private API Key"
+    echo "! 01. Check > https://github.com/ssokka/ubuntu/tree/master/.oci#oci-private-api-key"
+    cyn
 fi
 
 docker_file="$work_dir/dockerfile"
@@ -71,32 +71,32 @@ docker_hostname="`hostname`-oci-docker"
 docker_hostname="${docker_hostname^^}"
 
 if [[ -z `docker images -q "$docker_image"` ]]; then
-	log "Build OCI Docker Image"
-	cat > "$docker_file" << EOF
+    log "Build OCI Docker Image"
+    cat > "$docker_file" << EOF
 FROM ubuntu
 RUN sed -i -r 's/archive.ubuntu.com|security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list && \\
-	apt update -yqq && \\
-	apt install -yqq --no-install-recommends python3 python3-pip && \\
-	pip install --no-cache-dir --upgrade pip && \\
-	pip install --no-cache-dir oci discord_webhook
+    apt update -yqq && \\
+    apt install -yqq --no-install-recommends python3 python3-pip && \\
+    pip install --no-cache-dir --upgrade pip && \\
+    pip install --no-cache-dir oci discord_webhook
 EOF
-	docker build -t "$docker_image" "$work_dir"
+    docker build -t "$docker_image" "$work_dir"
 fi
 
 if [[ -z `docker ps -a --format {{.Names}} | grep -w "$docker_name"` ]]; then
-	log "Run OCI Docker Container"
-	docker run -dit --name "$docker_name" \
-	--hostname "$docker_hostname" \
-	-v ~/.oci:/root/.oci:ro \
-	-v /etc/localtime:/etc/localtime:ro \
-	-v /etc/resolv.conf:/etc/resolv.conf:ro \
-	--log-opt max-size=10m \
-	"$docker_image"
+    log "Run OCI Docker Container"
+    docker run -dit --name "$docker_name" \
+    --hostname "$docker_hostname" \
+    -v ~/.oci:/root/.oci:ro \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /etc/resolv.conf:/etc/resolv.conf:ro \
+    --log-opt max-size=10m \
+    "$docker_image"
 else
-	if [[ -z `docker container inspect -f '{{.State.Running}}' "$docker_name" | grep -w "true"` ]]; then
-		log "Start OCI Docker Container"
-		docker start "$docker_name" &> /dev/null
-	fi
+    if [[ -z `docker container inspect -f '{{.State.Running}}' "$docker_name" | grep -w "true"` ]]; then
+        log "Start OCI Docker Container"
+        docker start "$docker_name" &> /dev/null
+    fi
 fi
 
 log "Download & Run Python Script"
